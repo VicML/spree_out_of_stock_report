62233216fe89a03d1a0495f0e2360070361abf7a
Víctor Magaña <vmagana@gmail.com>
2017-09-26 01:26:06 -0300

    Create Controller, View and Route to Out of Stock report

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Spree::Core::Engine.add_routes do
   # Add your extension routes here
+  namespace :admin do
+    resources :reports, only: [:index, :show] do
+      collection do
+        get :sales_total
+        post :sales_total
+        get :out_of_stock
+        post :out_of_stock
+      end
+    end
+  end  
 end
